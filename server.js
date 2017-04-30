42be503a43aa0876ecb4a976ad03224c2577876a
Bryan Housel <bryan@mapbox.com>
2017-04-30 01:08:06 -0400

    Calculate values instead of hardcoding

--- server.js
+++ server.js
@@ -10,20 +10,22 @@ qspatch(app, 'first');
 router
     .get('/label', ctx => {
         var qs = ctx.request.query;
-        var pad = 8;
         var text = qs.text || 'bug';
         var fontfamily = qs.fontfamily || 'Helvetica,Arial';
         var fontsize = +qs.fontsize || 12;
         var fontweight = +qs.fontweight || 600;
-        var fontsizeadjust = fontsize + (fontweight > 500 ? 1 : 0);
-        var width = qs.width || pixelWidth(text, { size: fontsizeadjust }) + pad;
+        var fontsizeadjust = Math.round(fontsize * (fontweight > 500 ? 1.1 : 1));
+        var pad = Math.round(fontsize * 0.75);
+        var width = qs.width || Math.round(pixelWidth(text, { size: fontsizeadjust }) + pad);
+        var height = Math.round(fontsize * 1.6666);
+        var roundness = Math.round(height * 0.1);
         var fgcolor = qs.fgcolor || '#ffffff';
         var bgcolor = qs.bgcolor || '#ee0701';
 
         ctx.body =
-'<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="' + width + '" height="20">' +
-'<rect width="' + width +'" height="20" rx="2" ry="2" fill="' + bgcolor + '"/>' +
-'<text x="' + width / 2 + '" y="14" fill="' + fgcolor + '" text-anchor="middle" ' +
+'<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="' + width + '" height="' + height + '">' +
+'<rect width="' + width +'" height="' + height + '" rx="' + roundness + '" ry="' + roundness + '" fill="' + bgcolor + '"/>' +
+'<text x="' + Math.round(width / 2) + '" y="' + Math.round(height * 0.7) + '" fill="' + fgcolor + '" text-anchor="middle" ' +
    'font-family="' + fontfamily + '" font-size="' + fontsize + '" font-weight="' + fontweight + '">' +
  text + '</text>' +
 '</svg>';
