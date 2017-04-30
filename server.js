2ded21e64714e11e47ab60219ddbb02c24fd5c4d
Bryan Housel <bryan@mapbox.com>
2017-04-30 00:43:59 -0400

    Add querystring parameters, estimate text width

--- server.js
+++ server.js
@@ -1,20 +1,27 @@
 var Koa = require('koa');
 var Router = require('koa-router');
+var qspatch = require('koa-qs');
+var pixelWidth = require('string-pixel-width');
 
 var app = new Koa();
 var router = new Router();
+qspatch(app, 'first');
 
 router
     .get('/label', ctx => {
-        var width = 80;
-        var text = 'Hello Koa';
-        var fgcolor = '#ffffff';
-        var bgcolor = '#ee0701';
-        var fontfamily = 'Helvetica,Arial';
-        var fontsize = 12;
-        var fontweight = 600;
+        var qs = ctx.request.query;
+        var pad = 8;
+        var text = qs.text || 'bug';
+        var fontfamily = qs.fontfamily || 'Helvetica,Arial';
+        var fontsize = +qs.fontsize || 12;
+        var fontweight = +qs.fontweight || 600;
+        var fontsizeadjust = fontsize + (fontweight > 500 ? 1 : 0);
+        var width = qs.width || pixelWidth(text, { size: fontsizeadjust }) + pad;
+        var fgcolor = qs.fgcolor || '#ffffff';
+        var bgcolor = qs.bgcolor || '#ee0701';
+
         ctx.body =
-'<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="100" height="20">' +
+'<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="' + width + '" height="20">' +
 '<rect width="' + width +'" height="20" rx="2" ry="2" fill="' + bgcolor + '"/>' +
 '<text x="' + width / 2 + '" y="14" fill="' + fgcolor + '" text-anchor="middle" ' +
    'font-family="' + fontfamily + '" font-size="' + fontsize + '" font-weight="' + fontweight + '">' +
