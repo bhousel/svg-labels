5e20592adf13c3a1ca413dd8068f15514d68e061
Bryan Housel <bhousel@gmail.com>
2020-12-18 15:37:57 -0500

    Update server.js / koa-router - change usage of '*' in routes to '(.*)' - import '@koa/router', which is the more maintained one

--- server.js
+++ server.js
@@ -1,27 +1,24 @@
 const Koa = require('koa');
-const Router = require('koa-router');
-const qspatch = require('koa-qs');
+const Router = require('@koa/router');
 const send = require('koa-send');
 const makeLabel = require('.');
 
 const app = new Koa();
 const router = new Router();
-qspatch(app, 'first');
+require('koa-qs')(app);
 
 router
-    .get('/svg', ctx => {
-        ctx.response.type = 'image/svg+xml';
-        ctx.response.body = makeLabel(ctx.request.query);
-    })
-    .get('/', async ctx => {
-        await send(ctx, '/docs/index.html');
-    })
-    .get('*', async ctx => {
-        await send(ctx, ctx.path, { root: __dirname + '/docs' });
-    });
+  .get('/svg', ctx => {
+    ctx.response.type = 'image/svg+xml';
+    ctx.response.body = makeLabel(ctx.request.query);
+  })
+  .get('/', async (ctx) => {
+    await send(ctx, '/docs/index.html');
+  })
+  .get('(.*)', async (ctx) => {
+    await send(ctx, ctx.path, { root: __dirname + '/docs' });
+  });
 
 app
-    .use(router.routes())
-    .listen(3000, function() {
-        console.log('Koa is listening to http://localhost:3000');
-    });
+  .use(router.routes())
+  .listen(3000, () => console.log('Koa is listening to http://localhost:3000') );
